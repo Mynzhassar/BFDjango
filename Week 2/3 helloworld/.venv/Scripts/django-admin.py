2efdaee8822a950bf69e3c323aaa6b5518e82880
Dias Mynzhassar <mynzhassar.forever5@gmail.com>
2020-01-26 18:45:43 +0600

    hw2 done

---
+++ Week 2/3 helloworld/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\диас\desktop\diastudy\django\week 2\helloworld\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
