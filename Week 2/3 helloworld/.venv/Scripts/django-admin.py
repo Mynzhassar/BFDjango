04790cef05545a081c1d391920a3523146962c33
Dias Mynzhassar <mynzhassar.forever5@gmail.com>
2020-01-26 23:26:17 +0600

    removed environment

--- Week 2/3 helloworld/.venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\диас\desktop\diastudy\django\week 2\helloworld\.venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
